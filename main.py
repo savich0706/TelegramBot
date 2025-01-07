45a04b96cd3351b870ddca5b3924be423513bbbb
Savchenko Roman <savich.r97@list.ru>
2025-01-07 14:01:05 +0300

    Бот умеет только присылать обратно все что мы ему отправили

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
-print('Hellow, word!')
-text = 'вносим изменения в файл'
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message
+
+
+TOKEN = '7967376653:AAFNc3mZmNM7IXYm2-4S6dNjuRtP4q_LsWs'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command(commands=['start']))
+async def start_command(message: Message):
+    await message.answer('Привет, я Бот, напиши что нибудь!')
+    
+
+@dp.message(Command(commands=['help']))
+async def help_command(message: Message):
+    await message.answer('Вы выбрали помощь')
+
+
+@dp.message()
+async def all_message(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except:
+        await message.reply(text='Данный тип апдейтов не поддерживается')
+    
+    
+    
+@dp.message(F.photo)
+async def process_photo_message(message: Message):
+    await message.reply_photo(message.photo[-1].file_id, caption='Это Ваше фото')
+  
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
 
